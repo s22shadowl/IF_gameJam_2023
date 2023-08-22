468cd2946c11957199395dfa0cbf30db0989db2b
s22shadowl <jjerry409@gmail.com>
2023-08-23 00:18:04 +0800

    adjust vite deploy base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "IF_gameJam_2023",
+  base: "/IF_gameJam_2023/",
   plugins: [react()],
 })
